dba6858ed89710aae5aaf39370bc4febde34e3dc
chase <averych@126.com>
2025-04-30 20:19:09 +0800

    feat(handwired): 更新 Dactyl CC 键盘配置
    - 修改键盘名称为 Dactyl_CC
    - 更新制造商信息为 chase- 调整 keymap布局，移除部分键位冲突

--- keyboards/handwired/dactyl_cc/keymaps/vial/keymap.c
+++ keyboards/handwired/dactyl_cc/keymaps/vial/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_CODE] = LAYOUT(
         // left hand
-        QK_BOOT,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,
+        QK_BOOT, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,
         KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,
-        KC_CAPS, KC_A,    KC_S,   KC_D,    KC_F,    KC_G,
+        KC_CAPS, KC_A,    KC_S,   KC_D,    KC_F,    _______,
         KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,
                  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
                                                                     KC_INS, KC_DEL,
