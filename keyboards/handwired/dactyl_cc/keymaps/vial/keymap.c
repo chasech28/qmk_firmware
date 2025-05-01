3f9a547866d833a4175c4fbef05432e56b621140
chase <averych@126.com>
2025-05-01 11:15:59 +0800

    feat(handwired/dactyl_cc/vial): 添加 Tap Dance 功能并更新键位布局
    - 在 keymap.c 中添加 Tap Dance声明和定义，实现单击 Esc、双击 Caps Lock 的功能
    - 更新 _BASE 层布局，将 Caps Lock 键替换为 Tap Dance 键
    - _CODE 层布局，移除大量键位
    - 在 rules.mk 中启用 TAP_DANCE 功能

--- keyboards/handwired/dactyl_cc/keymaps/vial/keymap.c
+++ keyboards/handwired/dactyl_cc/keymaps/vial/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 
+// Layer declarations
 enum layers {
     _BASE, // default layer
     _FIRST,
@@ -7,17 +8,29 @@ enum layers {
     _CODE
 };
 
+// Tap Dance declarations
+enum {
+    TD_ESC_CAPS,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         // left hand
-        KC_GRV,  KC_1,    KC_2,   KC_3,    KC_4,    KC_5,
-        KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,
-        KC_CAPS, KC_A,    KC_S,   KC_D,    KC_F,    LT(_CODE, KC_G),
-        KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,
-                 KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-                                                                    KC_INS, KC_DEL,
-                                                          KC_BSPC, KC_LCTL, KC_LGUI,
-                                                                            KC_LALT,
+        KC_GRV,          KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
+        TD(TD_ESC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    LT(_CODE, KC_G),
+        KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
+                         KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+                                                                                        KC_INS, KC_DEL,
+                                                                              KC_BSPC, KC_LCTL, KC_LGUI,
+                                                                                                KC_LALT,
 
         // right hand
                                   KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_F1,
@@ -31,23 +44,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_CODE] = LAYOUT(
         // left hand
-        QK_BOOT, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,
-        KC_TAB,  KC_Q,    KC_W,   KC_E,    KC_R,    KC_T,
-        KC_CAPS, KC_A,    KC_S,   KC_D,    KC_F,    _______,
-        KC_LSFT, KC_Z,    KC_X,   KC_C,    KC_V,    KC_B,
-                 KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-                                                                    KC_INS, KC_DEL,
-                                                          KC_BSPC, KC_LCTL, _______,
-                                                                            KC_LALT,
+        QK_BOOT, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
+                 _______, _______, _______, _______,
+                                                                        KC_INS, KC_DEL,
+                                                              KC_BSPC, KC_LCTL, KC_LGUI,
+                                                                                KC_LALT,
 
         // right hand
-                                  KC_6,    KC_7,    KC_8,    KC_9,   KC_0,   KC_F1,
-                                  KC_Y,    KC_F1,   KC_F2,   KC_F3,  KC_F4,  KC_BSLS,
-                                  KC_H,    KC_F5,   KC_F6,   KC_F7,  KC_F8,  KC_QUOT,
-                                  KC_N,    KC_F9,   KC_F10,  KC_F11, KC_F12, KC_RSFT,
-                                  KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,
+                                  _______, _______, _______, _______, _______, _______,
+                                  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
+                                  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
+                                  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
+                                  _______, _______, _______, _______,
         KC_HOME, KC_RCTL,
-        KC_END, KC_ENT,  KC_SPC,
+        KC_END,  KC_ENT,  KC_SPC,
         KC_RALT
     ),
 };
